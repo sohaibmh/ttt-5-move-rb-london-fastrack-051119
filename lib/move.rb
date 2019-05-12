08d8dce7f16d2891614f457539dce6e9f25c742e
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-05-12 12:18:47 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,37 +1,5 @@
-
-def welcome
-  puts "Welcome to Tic Tac Toe!"
+def input_to_index("")
+  "".to_i
 end
 
-def display_board(array)
-  if array.empty?
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-else
-  puts " #{array[0]} | #{array[1]} | #{array[2]} "
-  puts "-----------"
-  puts " #{array[3]} | #{array[4]} | #{array[5]} "
-  puts "-----------"
-  puts " #{array[6]} | #{array[7]} | #{array[8]} "
-  end
-end
-
-moves = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(moves)
-puts
-def move
-  puts "Where would you like to go?"
-  input = gets.strip
-  index = input = input.to_i
-end
-move
-
-def board(array, index, value)
-  array[index] = value
-end
-board(moves, 1, "X")
-moves
-display_board(moves)
+puts "1"
