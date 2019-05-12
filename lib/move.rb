531db398f66fd2eb03f3359fc8a80afe4b0342c7
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-05-12 12:51:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,20 @@
+def display_board(array)
+  if array.empty?
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+else
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+  end
+end
+
+
 def input_to_index(string)
   a = string.to_i
   a-1
